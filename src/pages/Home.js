6cd4ef95802cfe87c010792e36849674157de66a
andjela2607 <andja011@gmail.com>
2023-12-16 17:00:30 +0100

    home-page-structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Box } from '@mui/material';
+
+import HeroBanner from '../components/Home/HeroBanner';
+import SearchExercises from '../components/Home/SearchExercises';
+import Exercises from '../components/Home/Exercises';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
   )
 }
 
